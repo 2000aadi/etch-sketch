f23c5f1aa5ca410001605a490524ffa8d66db0e5
Aditya <2000.aadi@gmail.com>
2022-01-20 13:45:15 +0530

    some more changes

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // script.js
 
 const container = document.querySelector('#container');
-
+const header = document.querySelector('#header');
 const clearButton = document.createElement('button');
 clearButton.textContent = 'Clear';
-container.appendChild(clearButton);
+header.appendChild(clearButton);
 
 for (let i=0; i<16; i++){
     const innerContainer = document.createElement('div');
     innerContainer.setAttribute('id', 'innerContainer');
     for (let j=0; j<16; j++){
         let sq = document.createElement('div');
-        sq.setAttribute('style', 'width: 25px; height: 25px; border: 2px solid black');
+        sq.setAttribute('class', 'blocks');
+        sq.setAttribute('style', 'flex: 1 1 0; border: 2px solid black');
        // sq.textContent = +j + 1;
         innerContainer.appendChild(sq);
         sq.addEventListener("mouseover", () => {
-            sq.setAttribute('style', "background-color:yellow; width: 25px; height: 25px; border: 2px solid black");
+            sq.setAttribute('style', "background-color:yellow; flex: 1 1 0; border: 2px solid black");
         }, false);
     }
     container.appendChild(innerContainer);
@@ -25,7 +26,7 @@ clearButton.onclick = removeElements;
 
 function removeElements() {
     //console.log('remove elements called');
-    while(container.childElementCount > 1){
+    while(container.hasChildNodes()){
         container.removeChild(container.lastChild);
     }
 
@@ -36,13 +37,23 @@ function removeElements() {
         innerContainer.setAttribute('id', 'innerContainer');
         for (let j=0; j<gridSize; j++){
             let sq = document.createElement('div');
-            sq.setAttribute('style', 'width: 25px; height: 25px; border: 2px solid black');
+            sq.setAttribute('class', 'blocks');
+            sq.setAttribute('style', 'flex: 1 1 0; border: 2px solid black');
            // sq.textContent = +j + 1;
             innerContainer.appendChild(sq);
             sq.addEventListener("mouseover", () => {
-                sq.setAttribute('style', "background-color:yellow; width: 25px; height: 25px; border: 2px solid black");
+                sq.setAttribute('style', "background-color:yellow; flex: 1 1 0; border: 2px solid black");
             }, false);
         }
         container.appendChild(innerContainer);
     }
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
